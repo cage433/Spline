6ed3f5a5cbb4431617c4396c48220afbbadb87a8
Alex McGuire <cage433@gmail.com>
2010-08-29 00:33:38 +0100

    Interpolating surface

--- src/test/scala/kalahari/SurfaceTests.scala
+++ src/test/scala/kalahari/SurfaceTests.scala
@@ -20,4 +20,19 @@ class CurveTests extends FunSuite with TestUtils{
     assertDoubleEquals(4.0, curve(0.0))
   }
 
+  test("Curve extrapolation"){
+    val surface = Surface(
+      (0.0, 0.0, 0.0),
+      (0.0, 1.0, 1.0),
+      (1.0, 0.0, 2.0)
+    )
+    val grid = surface.fillInGaps
+    assertDoubleEquals(1.5, grid(1.0, 1.0))
+    assertDoubleEquals(0.0, grid(-1.0, 0.0))
+    assertDoubleEquals(0.0, grid(-1.0, -1.0))
+    assertDoubleEquals(0.0, grid(0.0, -1.0))
+    assertDoubleEquals(2.0, grid(10.0, 0.0))
+    assertDoubleEquals(1.0, grid(0.0, 10.0))
+  }
+
 }
