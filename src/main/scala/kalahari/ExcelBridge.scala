f29ea325ba0f29e75c8407b77df97fc849843234
Alex <Alex@kalahari-laptop.kal.kalahari.co.uk>
2010-09-06 22:47:08 +0100

    Added new function

--- src/main/scala/kalahari/ExcelBridge.scala
+++ src/main/scala/kalahari/ExcelBridge.scala
@@ -5,13 +5,15 @@ import org.boris.xlloop.FunctionServer
 import org.boris.xlloop.handler._
 import org.boris.xlloop.reflect._
 
+class ExcelBridge
+
 object ExcelBridge extends Application{
         // Create function server on the default port
         val fs = new FunctionServer()
 
         // Create a reflection function handler and add the Math methods
         val rfh = new ReflectFunctionHandler()
-        rfh.addMethods("Kalahari.", classOf[kalahari.Kalahari$])
+        rfh.addMethods("Kalahari.", classOf[Kalahari])
 
         // Create a function information handler to register our functions
         val firh = new FunctionInformationHandler()
--- src/main/scala/kalahari/MyAddIn.scala
+++ src/main/scala/kalahari/MyAddIn.scala
@@ -21,4 +21,51 @@ object Kalahari{
 		println("any arr = " + arr.mkString(", "))
 		(0.0 /: arr.filter(_.isInstanceOf[Double]).map(_.asInstanceOf[Double]))(_+_)
 	}
+
+	def splineSurface(
+		xAxis : Array[Double], 
+		yAxis : Array[Double], 
+		knownValues : Array[Array[Any]]) : Array[Array[Double]] = {
+		
+		splineSurfaceWithNewAxes(xAxis, yAxis, knownValues, xAxis, yAxis)
+	}
+
+	def splineSurfaceWithNewAxes(
+		originalXAxis : Array[Double],
+		originalyAxis : Array[Double],
+		knownValues : Array[Array[Any]],
+		newXAxis : Array[Double],
+		newYAxis : Array[Double]
+	) : Array[Array[Double]] = {
+
+		var points = List[(Double, Double, Double)]()
+		originalyAxis.zip(knownValues).foreach{
+			case (y, rowVec) =>
+				originalXAxis.zip(rowVec).foreach{
+					case (x, z) =>
+						if (z.isInstanceOf[Double])
+							points = (x, y, z.asInstanceOf[Double]) :: points
+			}
+		}
+		if (points.isEmpty){
+			Array.fill(newYAxis.size)(Array.fill(newXAxis.size)(0.0))
+		} else {
+			val surface = new Surface(points).fillInGaps
+			newYAxis.map{
+				y =>
+					newXAxis.map{
+						x =>
+							surface(x, y)
+					}
+			}
+		}
+	}
+	def inputMatrix(m : Array[Array[Any]]) : Double = {
+		m.foreach{
+			vec =>
+				println("Vec = " + vec.mkString(", "))
+		}
+		37
+	}
+		
 }
